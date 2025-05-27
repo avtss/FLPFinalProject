1f8129d0041646c9944bb64c9af27851350402a0
avtss <avetiszadikyann@mail.ru>
2025-05-27 20:56:12 +0300

    added games to db

--- games_db.pl
+++ games_db.pl
@@ -28,33 +28,99 @@ game('Rocket League Season 2', [n,y,n,n,y,n,y,n]).
 game('Tetris Effect', [n,n,n,n,n,n,n,n]).
 game('Call of Duty Warzone', [n,y,n,y,y,n,y,y]).
 game('Valorant Episode 3', [n,y,n,n,y,n,y,y]).
-game('Celestial Warriors',[y,n,y,n,y,n,y,n,y]).
-game('Shadow Realm',[y,y,n,n,y,y,n,y,n]).
-game('Frozen Tundra',[n,n,y,y,n,n,y,y,n]).
-game('Neon Racer',[n,y,y,n,y,n,y,n,y]).
-game('Quantum Leap',[y,n,n,y,y,n,n,y,y]).
-game('Silent Whisper',[n,y,n,y,n,y,y,n,y]).
-game('Iron Fortress',[y,y,y,n,y,n,n,y,n]).
-game('Crystal Odyssey',[n,y,y,y,n,y,n,n,y]).
-game('Lunar Eclipse',[y,n,y,y,n,y,n,y,n]).
-game('Storm Breaker',[y,y,n,y,y,n,y,n,n]).
-game('Dragon Forge',[y,n,y,n,n,y,y,y,n]).
-game('Galaxy Rangers',[n,n,y,y,y,n,y,n,y]).
-game('Mystic Hollow',[n,y,n,n,y,y,n,y,y]).
-game('Cyber Knights',[y,n,n,n,y,y,y,n,y]).
-game('Titanfall',[y,y,y,y,y,n,n,n,n]).
-game('Solar Winds',[n,n,n,y,n,y,y,y,n]).
-game('Phantom Strike',[y,y,n,n,n,y,n,y,y]).
-game('Blazing Trails',[n,y,y,n,y,n,n,y,y]).
-game('Echoes of Time',[y,n,y,y,y,n,y,n,n]).
-game('Vortex Arena',[n,y,n,y,y,y,n,n,y]).
-game('Neptune’s Wrath',[y,n,n,y,n,y,y,n,y]).
-game('Midnight Legends',[n,y,y,y,y,n,y,y,n]).
-game('Dragon’s Curse',[y,y,n,n,y,n,y,y,y]).
-game('Silent Thunder',[n,n,y,y,n,y,n,y,y]).
-game('Ironclad',[y,n,y,n,y,y,n,y,n]).
-game('Crystal Maze',[n,y,n,y,n,n,y,y,y]).
-game('Lunar Legends',[y,y,y,n,n,y,y,n,y]).
-game('Storm Chasers',[n,n,n,n,y,y,y,y,y]).
-game('Dragonfire',[y,n,y,y,n,n,n,y,y]).
-game('Galactic Crusade',[n,y,y,n,y,y,n,n,y]).
+game('Celestial Warriors', [y,n,y,n,y,n,y,n,y]).
+game('Shadow Realm', [y,y,n,n,y,y,n,y,n]).
+game('Frozen Tundra', [n,n,y,y,n,n,y,y,n]).
+game('Neon Racer', [n,y,y,n,y,n,y,n,y]).
+game('Quantum Leap', [y,n,n,y,y,n,n,y,y]).
+game('Silent Whisper', [n,y,n,y,n,y,y,n,y]).
+game('Iron Fortress', [y,y,y,n,y,n,n,y,n]).
+game('Crystal Odyssey', [n,y,y,y,n,y,n,n,y]).
+game('Lunar Eclipse', [y,n,y,y,n,y,n,y,n]).
+game('Storm Breaker', [y,y,n,y,y,n,y,n,n]).
+game('Dragon Forge', [y,n,y,n,n,y,y,y,n]).
+game('Galaxy Rangers', [n,n,y,y,y,n,y,n,y]).
+game('Mystic Hollow', [n,y,n,n,y,y,n,y,y]).
+game('Cyber Knights', [y,n,n,n,y,y,y,n,y]).
+game('Titanfall', [y,y,y,y,y,n,n,n,n]).
+game('Solar Winds', [n,n,n,y,n,y,y,y,n]).
+game('Phantom Strike', [y,y,n,n,n,y,n,y,y]).
+game('Blazing Trails', [n,y,y,n,y,n,n,y,y]).
+game('Echoes of Time', [y,n,y,y,y,n,y,n,n]).
+game('Vortex Arena', [n,y,n,y,y,y,n,n,y]).
+game('Neptune’s Wrath', [y,n,n,y,n,y,y,n,y]).
+game('Midnight Legends', [n,y,y,y,y,n,y,y,n]).
+game('Dragon’s Curse', [y,y,n,n,y,n,y,y,y]).
+game('Silent Thunder', [n,n,y,y,n,y,n,y,y]).
+game('Ironclad', [y,n,y,n,y,y,n,y,n]).
+game('Crystal Maze', [n,y,n,y,n,n,y,y,y]).
+game('Lunar Legends', [y,y,y,n,n,y,y,n,y]).
+game('Storm Chasers', [n,n,n,n,y,y,y,y,y]).
+game('Dragonfire', [y,n,y,y,n,n,n,y,y]).
+game('Galactic Crusade', [n,y,y,n,y,y,n,n,y]).
+game('Bloodborne', [y,y,y,n,y,n,y,n]).
+game('Dead Cells', [n,y,y,n,y,n,y,y]).
+game('Inside', [n,n,y,n,y,n,y,n]).
+game('Celestial Frontier', [y,n,y,y,n,y,n,n]).
+game('Frostpunk', [y,y,n,y,n,n,y,n]).
+game('Subnautica', [n,y,n,y,n,n,y,n]).
+game('Slay the Spire', [n,y,y,y,n,n,y,n]).
+game('Hollow Frontier', [y,n,n,y,y,n,n,y]).
+game('Death Stranding', [y,y,n,y,y,n,n,n]).
+game('Ori and the Blind Forest', [n,y,y,n,n,y,y,n]).
+game('Cuphead', [n,n,y,n,y,y,n,y]).
+game('Little Nightmares', [n,n,y,n,n,y,y,n]).
+game('Terraria', [n,y,n,n,y,y,n,n]).
+game('Dying Light', [y,n,y,y,n,n,y,n]).
+game('Overwatch', [n,y,n,y,y,n,y,n]).
+game('The Division 2', [y,n,n,y,y,n,y,n]).
+game('No Man’s Sky', [n,y,n,n,y,y,n,y]).
+game('Control', [y,n,y,y,n,y,n,y]).
+game('Watch Dogs Legion', [y,n,y,n,y,n,y,y]).
+game('Rainbow Six Siege', [n,y,n,y,n,y,y,n]).
+game('Among Trees', [n,y,n,n,n,y,y,n]).
+game('Journey', [n,n,y,n,n,y,n,y]).
+game('Dead by Daylight', [n,y,n,y,y,n,n,y]).
+game('Metro Exodus', [y,y,n,y,y,n,n,y]).
+game('The Outer Worlds', [y,y,y,n,n,y,y,n]).
+game('Ghostrunner', [n,y,n,y,n,y,y,y]).
+game('Forza Horizon 5', [n,y,n,n,y,y,y,n]).
+game('Control Ultimate Edition', [y,n,y,y,y,n,n,y]).
+game('Spiritfarer', [n,y,n,n,n,y,y,y]).
+game('Cuphead DLC', [n,n,y,n,y,y,y,n]).
+game('Super Meat Boy', [n,n,y,n,y,n,y,y]).
+game('Subnautica Below Zero', [n,y,n,y,n,y,n,y]).
+game('Among Us VR', [n,n,n,y,y,n,y,n]).
+game('Hollow Knight Silksong', [n,y,y,n,y,n,y,n]).
+game('Celeste DLC', [n,n,y,n,n,y,y,y]).
+game('Nebula Quest', [n,y,y,n,y,n,n,y])
+game('Void Runner', [y,n,y,n,y,n,y,n])
+game('Solaris Prime', [y,y,n,n,y,n,y,n])
+game('Crystal Veil', [n,n,y,y,n,y,n,y])
+game('Silent Horizon', [n,n,y,y,y,n,n,y])
+game('Dragonspire', [y,n,y,y,n,n,y,y])
+game('Arcane Pulse', [y,n,y,n,y,y,n,n])
+game('Vortex Surge', [n,y,n,y,n,n,y,y])
+game('Echo Realm', [y,y,y,n,n,y,n,y])
+game('Frostveil', [n,n,y,y,y,n,y,n])
+game('Nebula Raiders', [y,n,n,y,n,y,y,y])
+game('Celestial Drift', [y,y,n,n,n,y,y,n])
+game('Voidcaller’s Path', [n,n,y,y,n,y,y,y])
+game('Neon Shadows', [y,y,n,y,n,n,y,y])
+game('Echoes of Arcana', [n,y,y,n,n,y,n,y])
+game('Shadowfall Dominion', [y,y,n,y,y,n,y,n])
+game('Lunar Rift', [y,n,y,y,n,y,y,n])
+game('Celestial Rift', [n,n,y,y,y,n,y,y])
+game('Echo Sentinel', [n,n,y,y,n,y,y,n])
+game('Shadowspire', [y,y,n,n,y,n,y,y])
+game('Solar Nexus', [n,y,y,n,n,y,y,y])
+game('Ironbane', [y,n,n,y,y,n,y,y])
+game('Nebula Vanguard', [n,y,y,y,n,y,n,y])
+game('Stormwatch', [y,y,n,y,n,y,n,n])
+game('Vortex Phantom', [y,n,n,n,y,y,n,y])
+game('Nebula Ascend', [y,y,n,n,y,y,n,y])
+game('Crystal Rift', [y,n,y,n,n,y,y,y])
+game('Ironclad Saga', [n,y,y,y,y,n,n,y])
+game('Starlight Chronicles', [y,y,n,n,y,y,n,n])
+game('Stormbreaker', [n,y,y,y,n,n,y,y])
+game('Dragonspire', [y,n,y,y,y,n,y,n])
